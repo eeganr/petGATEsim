8d0155b1e9e737be2e05800817f67fdc4a417350
Eegan Ram <eeganr@sh04-ln02.stanford.edu>
2025-05-15 13:01:38 -0700

    fixed scripts

---
+++ submit1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#SBATCH --job-name=PhytoPET
+#
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=5G
+#SBATCH --mail-type=BEGIN,FAIL,END
+
+index=$1
+source=$2
+duration=$3
+rotation=$4
+
+# -B arg binds path to image / -c executes ...gate.sh file (seems like -c is redundant but kept here because it worked)
+
+# ###################### EDIT ##################################
+# change all refereneces to /home/users/fwdesai/PhytoPET to the directory where you keep the simulation files
+# do not change the path /home/groups/cslevin/mhchin/gate/gate_latest.sif
+# make sure you are calling runtrain1.sh as done below
+################################################################
+
+singularity exec -B /home/users/eeganr/petGATEsim:/home/users/eeganr/petGATEsim /home/groups/cslevin/mhchin/gate/gate_latest.sif /home/users/eeganr/petGATEsim/runtrain1.sh $index $source $duration $rotation
+
