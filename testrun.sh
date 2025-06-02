fe237792157da5e738854aee07d5e80403f89470
Eegan Ram <67843748+eeganr@users.noreply.github.com>
2025-06-01 21:53:04 -0700

    testrun script paths

--- testrun.sh
+++ testrun.sh
@@ -8,4 +8,4 @@
 #SBATCH --mem-per-cpu=5G
 #SBATCH --mail-type=BEGIN,FAIL,END
 
-singularity exec -B /home/users/eeganr/petGATEsim:/home/users/eeganr/petGATEsim /home/groups/cslevin/mhchin/gate/gate_latest.sif /home/users/eeganr/petGATEsim/runtrain1.sh $1 /home/users/eeganr/petGATEsim/testcylinder.mac 60.0 /home/users/eeganr/petGATEsim/Geometry.mac
+singularity exec -B /home/users/eeganr/petGATEsim:/home/users/eeganr/petGATEsim /home/groups/cslevin/mhchin/gate/gate_latest.sif /home/users/eeganr/petGATEsim/runtrain1.sh $1 /home/users/eeganr/petGATEsim/macros/testcylinder.mac 1.0 /home/users/eeganr/petGATEsim/macros/Geometry.mac
