8e1e6ec6f17d9bd7d4cc2582f0ac30c05be47058
Eegan Ram <67843748+eeganr@users.noreply.github.com>
2025-06-01 19:06:00 -0700

    new data script

--- testrun.sh
+++ testrun.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+#
+#SBATCH --job-name=GetData
+#
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=5G
+#SBATCH --mail-type=BEGIN,FAIL,END
 
-singularity exec -B /home/users/eeganr/petGATEsim:/home/users/eeganr/petGATEsim /home/groups/cslevin/mhchin/gate/gate_latest.sif /home/users/eeganr/petGATEsim/runtrain1.sh $1 /home/users/eeganr/petGATEsim/testcylinder.mac 60.0 /home/users/eeganr/petGATEsim/Geometry.mac
+
+for i in $(seq 1 30);
+do
+    singularity exec -B /home/users/eeganr/petGATEsim:/home/users/eeganr/petGATEsim /home/groups/cslevin/mhchin/gate/gate_latest.sif /home/users/eeganr/petGATEsim/runtrain1.sh $i /home/users/eeganr/petGATEsim/testcylinder.mac 60.0 /home/users/eeganr/petGATEsim/Geometry.mac
+done
