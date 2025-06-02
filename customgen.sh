2e3fb4e465f23ac4f5858deb679fc0cee71a96f4
Eegan Ram <67843748+eeganr@users.noreply.github.com>
2025-06-01 19:07:52 -0700

    better scripts

--- customgen.sh
+++ customgen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=PhytoPET
+#SBATCH --job-name=GetData
 #
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
@@ -8,7 +8,8 @@
 #SBATCH --mem-per-cpu=5G
 #SBATCH --mail-type=BEGIN,FAIL,END
 
+
 for i in $(seq 1 30);
 do
-    echo $i
+    singularity exec -B /home/users/eeganr/petGATEsim:/home/users/eeganr/petGATEsim /home/groups/cslevin/mhchin/gate/gate_latest.sif /home/users/eeganr/petGATEsim/runtrain1.sh $i /home/users/eeganr/petGATEsim/testcylinder.mac 60.0 /home/users/eeganr/petGATEsim/Geometry.mac
 done
--- testrun.sh
+++ testrun.sh
@@ -2,14 +2,10 @@
 #
 #SBATCH --job-name=GetData
 #
-#SBATCH --time=12:00:00
+#SBATCH --time=48:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=5G
 #SBATCH --mail-type=BEGIN,FAIL,END
 
-
-for i in $(seq 1 30);
-do
-    singularity exec -B /home/users/eeganr/petGATEsim:/home/users/eeganr/petGATEsim /home/groups/cslevin/mhchin/gate/gate_latest.sif /home/users/eeganr/petGATEsim/runtrain1.sh $i /home/users/eeganr/petGATEsim/testcylinder.mac 60.0 /home/users/eeganr/petGATEsim/Geometry.mac
-done
+singularity exec -B /home/users/eeganr/petGATEsim:/home/users/eeganr/petGATEsim /home/groups/cslevin/mhchin/gate/gate_latest.sif /home/users/eeganr/petGATEsim/runtrain1.sh $1 /home/users/eeganr/petGATEsim/testcylinder.mac 60.0 /home/users/eeganr/petGATEsim/Geometry.mac
