ebd4bc0ae46feead47a24fb89e1c9fdd5b58a8f0
Eegan Ram <67843748+eeganr@users.noreply.github.com>
2025-06-23 10:44:42 -0700

    fixed styling

--- analysis/findrandoms.py
+++ analysis/findrandoms.py
@@ -5,9 +5,9 @@ from scipy.optimize import root_scalar
 import uproot
 
 # === CONFIG ===
-TIME = 60.0 # total sim time (s)
-TAU = 1.2e-8 # coincidence window (s)
-DELAY = TAU # delay for DW estimate (s)
+TIME = 60.0  # total sim time (s)
+TAU = 1.2e-8  # coincidence window (s)
+DELAY = TAU  # delay for DW estimate (s)
 DETECTORS = 48 * 48
 PATH_PREFIX = "/scratch/users/eeganr/pastoutput/output"
 PATH_SUFFIX = ".root"
@@ -17,6 +17,9 @@ FILE_RANGE = range(1, 100)
 
 # Read in data from ROOT files
 def read_root_file(infile):
+
+    # Reads appropriate data field for a given file name
+    # (e.g. file name in form Singles;16 for first 16k events)
     def get_all_vals(file, name):
         num = max([int(i.split(';')[1]) for i in file.keys() if i.split(';')[0] == name])
         return file[f'{name};{num}']
@@ -28,51 +31,70 @@ def read_root_file(infile):
         singles = pd.DataFrame({
             "time": singles_tree["time"].array(library="np"),
             "detector": singles_tree["crystalID"].array(library="np"),
-            "source": list(map(tuple, np.stack((singles_tree["sourcePosX"].array(library="np"), 
-                                singles_tree["sourcePosY"].array(library="np"), 
-                                singles_tree["sourcePosZ"].array(library="np")), axis=-1))),
+            "source": list(map(tuple, np.stack((
+                singles_tree["sourcePosX"].array(library="np"),
+                singles_tree["sourcePosY"].array(library="np"),
+                singles_tree["sourcePosZ"].array(library="np")), axis=-1))),
         })
         coincidences = pd.DataFrame({
             "time1": coincidence_tree["time1"].array(library="np"),
             "time2": coincidence_tree["time2"].array(library="np"),
             "detector1": coincidence_tree["crystalID1"].array(library="np"),
             "detector2": coincidence_tree["crystalID2"].array(library="np"),
-            "source1": list(map(tuple, np.stack((coincidence_tree["sourcePosX1"].array(library="np"), 
-                                coincidence_tree["sourcePosY1"].array(library="np"), 
-                                coincidence_tree["sourcePosZ1"].array(library="np")), axis=-1))),
-            "source2": list(map(tuple, np.stack((coincidence_tree["sourcePosX2"].array(library="np"), 
-                                coincidence_tree["sourcePosY2"].array(library="np"), 
-                                coincidence_tree["sourcePosZ2"].array(library="np")), axis=-1))),
+            "source1": list(map(tuple, np.stack((
+                coincidence_tree["sourcePosX1"].array(library="np"),
+                coincidence_tree["sourcePosY1"].array(library="np"),
+                coincidence_tree["sourcePosZ1"].array(library="np")), axis=-1))),
+            "source2": list(map(tuple, np.stack((
+                coincidence_tree["sourcePosX2"].array(library="np"),
+                coincidence_tree["sourcePosY2"].array(library="np"),
+                coincidence_tree["sourcePosZ2"].array(library="np")), axis=-1))),
         })
         coincidences['true'] = coincidences['source1'] == coincidences['source2']
 
     # Calculate singles/prompts counts
-    det1_counts = coincidences['detector1'].value_counts().to_dict() # det1 coincidences involved
-    det2_counts = coincidences['detector2'].value_counts().to_dict() # det2 coincidences involved
+    det1_counts = coincidences['detector1'].value_counts().to_dict()  # det1 coincidences involved
+    det2_counts = coincidences['detector2'].value_counts().to_dict()  # det2 coincidences involved
+
+    # Prompts are the sum of all coincidences involving a given detector,
+    # regardless of first or second
+    # Every detector, regardless of presence, has a key
     prompts = pd.DataFrame({'detector': list(range(DETECTORS))})
-    prompts['prompts'] = prompts['detector'].map(lambda x: det1_counts.get(x, 0) + det2_counts.get(x, 0))
+    prompts['prompts'] = prompts['detector'].map(
+        lambda x: det1_counts.get(x, 0) +
+        det2_counts.get(x, 0)
+    )
+
+    # dicts of form {detectorID: count}
     prompts_count = prompts.set_index('detector')['prompts'].to_dict()
     singles_count = singles['detector'].value_counts().to_dict()
     return singles, coincidences, singles_count, prompts_count
 
-# Returns the randoms rate from a pair of detectors with crystalIDs i and j
+
+# Returns the Singles-Prompts (SP) randoms rate estimate from a pair
+# of detectors with crystalIDs i and j per Oliver & Rafecas
 def randomsrate(i, j, singles_count, prompts_count, L, S):
+    # P_i and P_j are prompts rates for detectors i, j
     P_i = prompts_count.get(i, 0) / TIME
     P_j = prompts_count.get(j, 0) / TIME
+    # S_i and S_j are singles rates for detectors i, j
     S_i = singles_count.get(i, 0) / TIME
     S_j = singles_count.get(j, 0) / TIME
+    # Coefficient for the randoms rate equation
     coeff = (2 * TAU * np.exp(-(L + S)*TAU))/((1 - 2 * L * TAU)**2)
+    # Further terms in SP equation
     i_term = S_i - np.exp((L + S)*TAU) * P_i
     j_term = S_j - np.exp((L + S)*TAU) * P_j
     return coeff * i_term * j_term
 
-# Calculate singles-prompts estimate of total randoms rate
+
+# Calculate Singles-Prompts (SP) estimate of total randoms rate
 def singles_prompts(singles_count, prompts_count, singles, coincidences):
 
-    S = len(singles) / TIME # Rate of singles measured by scanner as a whole
-    P = 2 * len(coincidences) / TIME # Twice the prompts rate
-    
-    # Roots of this function are the lambda (L) values.
+    S = len(singles) / TIME  # Rate of singles measured by scanner as a whole
+    P = 2 * len(coincidences) / TIME  # Twice the prompts rate
+
+    # Roots of this function are the lambda (L) values needed for the SP estimate.
     def lambda_eq(L):
         return 2 * TAU * L * L - L + S - P * np.exp((L + S)*TAU)
     L = root_scalar(lambda_eq, x0=0)
@@ -80,18 +102,25 @@ def singles_prompts(singles_count, prompts_count, singles, coincidences):
         raise RuntimeError("Failed to converge on lambda.")
     L = L.root
 
+    # Calculate the Singles-Prompts rate estimate for the whole scanner
+    # summing over all pairs of detectors
     sp_rate = 0
     for i in range(DETECTORS):
         for j in range(i, DETECTORS):
             sp_rate += randomsrate(i, j, singles_count, prompts_count, L, S)
     return sp_rate * TIME
 
+
 # Calculate delayed-window estimate of total randoms rate
 def delayed_window(singles):
     dw_estimate = 0
     for t in singles['time']:
-        dw_estimate += np.searchsorted(singles['time'], t + DELAY + TAU) - np.searchsorted(singles['time'], t + TAU)
+        dw_estimate += (
+            np.searchsorted(singles['time'], t + DELAY + TAU) -
+            np.searchsorted(singles['time'], t + TAU)
+        )
     return dw_estimate
+
 
 # Calculate singles-rate estimate of total randoms rate
 def singles_rate(singles_count):
@@ -101,6 +130,11 @@ def singles_rate(singles_count):
             sr_rate += 2 * TAU * singles_count.get(i, 0) / TIME * singles_count.get(j, 0) / TIME
     return sr_rate * TIME
 
+
+# Main function to read files and calculate estimates for many files of form
+# output1.root, output2.root, ..., outputN.root
+# Range [1, N) defined in FILE_RANGE
+# Writes results to estimations.csv
 if __name__ == "__main__":
     sp, dw, sr, actual = [], [], [], []
     for i in FILE_RANGE:
@@ -110,8 +144,8 @@ if __name__ == "__main__":
         sp.append(singles_prompts(singles_count, prompts_count, singles, coincidences))
         dw.append(delayed_window(singles))
         sr.append(singles_rate(singles_count))
-        actual.append(len(coincidences[coincidences['true'] == False]))
-    
+        actual.append(len(coincidences[coincidences['true'] is False]))
+
     df = pd.DataFrame({'sp': sp, 'dw': dw, 'sr': sr, 'actual': actual})
     with open('estimations.csv', 'w') as f:
         df.to_csv(f)
--- analysis/singlesprompts.py
+++ analysis/singlesprompts.py
@@ -5,9 +5,9 @@ from scipy.optimize import root_scalar
 import uproot
 
 # === CONFIG ===
-TIME = 60.0 # total sim time (s)
-TAU = 1.2e-8 # coincidence window (s)
-DELAY = TAU # delay for DW estimate (s)
+TIME = 60.0  # total sim time (s)
+TAU = 1.2e-8  # coincidence window (s)
+DELAY = TAU  # delay for DW estimate (s) TODO: make this much longer check with nasir
 DETECTORS = 48 * 48
 PATH_PREFIX = "/scratch/users/eeganr/pastoutput/output"
 PATH_SUFFIX = ".root"
@@ -28,32 +28,39 @@ def read_root_file(infile):
         singles = pd.DataFrame({
             "time": singles_tree["time"].array(library="np"),
             "detector": singles_tree["crystalID"].array(library="np"),
-            "source": list(map(tuple, np.stack((singles_tree["sourcePosX"].array(library="np"), 
-                                singles_tree["sourcePosY"].array(library="np"), 
-                                singles_tree["sourcePosZ"].array(library="np")), axis=-1))),
-        })
+            "source": list(map(tuple,
+                               np.stack((singles_tree["sourcePosX"].array(library="np"),
+                                        singles_tree["sourcePosY"].array(library="np"),
+                                        singles_tree["sourcePosZ"].array(library="np")), axis=-1)))
+                                })
         coincidences = pd.DataFrame({
             "time1": coincidence_tree["time1"].array(library="np"),
             "time2": coincidence_tree["time2"].array(library="np"),
             "detector1": coincidence_tree["crystalID1"].array(library="np"),
             "detector2": coincidence_tree["crystalID2"].array(library="np"),
-            "source1": list(map(tuple, np.stack((coincidence_tree["sourcePosX1"].array(library="np"), 
-                                coincidence_tree["sourcePosY1"].array(library="np"), 
+            "source1": list(map(tuple, np.stack((
+                                coincidence_tree["sourcePosX1"].array(library="np"),
+                                coincidence_tree["sourcePosY1"].array(library="np"),
                                 coincidence_tree["sourcePosZ1"].array(library="np")), axis=-1))),
-            "source2": list(map(tuple, np.stack((coincidence_tree["sourcePosX2"].array(library="np"), 
-                                coincidence_tree["sourcePosY2"].array(library="np"), 
+            "source2": list(map(tuple, np.stack((
+                                coincidence_tree["sourcePosX2"].array(library="np"),
+                                coincidence_tree["sourcePosY2"].array(library="np"),
                                 coincidence_tree["sourcePosZ2"].array(library="np")), axis=-1))),
         })
         coincidences['true'] = coincidences['source1'] == coincidences['source2']
 
     # Calculate singles/prompts counts
-    det1_counts = coincidences['detector1'].value_counts().to_dict() # det1 coincidences involved
-    det2_counts = coincidences['detector2'].value_counts().to_dict() # det2 coincidences involved
+    det1_counts = coincidences['detector1'].value_counts().to_dict()  # det1 coincidences involved
+    det2_counts = coincidences['detector2'].value_counts().to_dict()  # det2 coincidences involved
     prompts = pd.DataFrame({'detector': list(range(DETECTORS))})
-    prompts['prompts'] = prompts['detector'].map(lambda x: det1_counts.get(x, 0) + det2_counts.get(x, 0))
+    prompts['prompts'] = prompts['detector'].map(
+        lambda x: det1_counts.get(x, 0) +
+        det2_counts.get(x, 0)
+    )
     prompts_count = prompts.set_index('detector')['prompts'].to_dict()
     singles_count = singles['detector'].value_counts().to_dict()
     return singles, coincidences, singles_count, prompts_count
+
 
 # Returns the randoms rate from a pair of detectors with crystalIDs i and j
 def randomsrate(i, j, singles_count, prompts_count, L, S):
@@ -66,12 +73,13 @@ def randomsrate(i, j, singles_count, prompts_count, L, S):
     j_term = S_j - np.exp((L + S)*TAU) * P_j
     return coeff * i_term * j_term
 
+
 # Calculate singles-prompts estimate of total randoms rate
 def singles_prompts(singles_count, prompts_count, singles, coincidences):
 
-    S = len(singles) / TIME # Rate of singles measured by scanner as a whole
-    P = 2 * len(coincidences) / TIME # Twice the prompts rate
-    
+    S = len(singles) / TIME  # Rate of singles measured by scanner as a whole
+    P = 2 * len(coincidences) / TIME  # Twice the prompts rate
+
     # Roots of this function are the lambda (L) values.
     def lambda_eq(L):
         return 2 * TAU * L * L - L + S - P * np.exp((L + S)*TAU)
@@ -86,12 +94,15 @@ def singles_prompts(singles_count, prompts_count, singles, coincidences):
             sp_rate += randomsrate(i, j, singles_count, prompts_count, L, S)
     return sp_rate * TIME
 
+
 # Calculate delayed-window estimate of total randoms rate
 def delayed_window(singles):
     dw_estimate = 0
     for t in singles['time']:
-        dw_estimate += np.searchsorted(singles['time'], t + DELAY + TAU) - np.searchsorted(singles['time'], t + TAU)
+        dw_estimate += (np.searchsorted(singles['time'], t + DELAY + TAU)
+                        - np.searchsorted(singles['time'], t + TAU))
     return dw_estimate
+
 
 # Calculate singles-rate estimate of total randoms rate
 def singles_rate(singles_count):
@@ -100,5 +111,6 @@ def singles_rate(singles_count):
         for j in range(i, DETECTORS):
             sr_rate += 2 * TAU * singles_count.get(i, 0) / TIME * singles_count.get(j, 0) / TIME
     return sr_rate * TIME
+
 
 infile = PATH_PREFIX + str(0) + PATH_SUFFIX
