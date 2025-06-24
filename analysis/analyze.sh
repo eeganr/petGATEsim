da548271e4a6b8f21bb70b5fdaaffb0b3d617090
eeganr <eeganr@stanford.edu>
2025-06-24 15:37:52 -0700

    new options

--- analysis/analyze.sh
+++ analysis/analyze.sh
@@ -2,11 +2,11 @@
 #
 #SBATCH --job-name=analyze
 #
-#SBATCH --time=3:00:00
+#SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=5G
 #SBATCH --mail-type=BEGIN,FAIL,END
 
 module load python/3.12
-python3 /home/users/eeganr/petGATEsim/analysis/findrandoms.py
+python3 findrandoms.py -o estimates1mCi -t 1
--- analysis/findrandoms.py
+++ analysis/findrandoms.py
@@ -1,23 +1,42 @@
 import numpy as np
 import pandas as pd
 from scipy.optimize import root_scalar
-# import struct
 import uproot
+import argparse
+
 
 # === CONFIG ===
-TIME = 60.0  # total sim time (s)
 CYCLE = 1.6e-9  # clock cycle (s)
 TAU = 3 * CYCLE  # coincidence window (s)
 DELAY = 10 * CYCLE  # delay for DW estimate (s)
 DETECTORS = 48 * 48
-PATH_PREFIX = "/scratch/users/eeganr/pastoutput/output"
+PATH_PRE_PREFIX = "/scratch/users/eeganr/"
 PATH_SUFFIX = ".root"
 FILE_RANGE = range(1, 100 + 1)
 # === END CONFIG ===
 
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filter", action="store_true", help="whether to filter energy")
+parser.add_argument("-p", "--path", type=str, default="june23output/output", help="path prefix for input files")
+parser.add_argument("-o", "--output", type=str, default="estimations.csv", help="output file name")
+parser.add_argument("-t", "--time", type=float, default=60.0, help="total simulation time in seconds")
+args = parser.parse_args()
 
-# Read in data from ROOT files
+PATH_PREFIX = PATH_PRE_PREFIX + args.path
+FILTER_ENERGY = args.filter
+OUTPUT_FILE = args.output + ".csv"
+TIME = args.time
+if FILTER_ENERGY:
+    print("Filtering singles by energy in range 0.450 to 0.750 MeV")
+print(f"Output will be written to {OUTPUT_FILE}")
+
+
 def read_root_file(infile):
+    """
+        Read in data from singles ROOT file, returns a DataFrame with columns:
+        time, detector ID, source
+    """    
 
     # Reads appropriate data field for a given file name
     # (e.g. file name in form Singles;16 for first 16k events)
@@ -27,7 +46,6 @@ def read_root_file(infile):
 
     with uproot.open(infile) as file:
         singles_tree = get_all_vals(file, 'Singles')
-        coincidence_tree = get_all_vals(file, 'Coincidences')
 
         singles = pd.DataFrame({
             "time": singles_tree["time"].array(library="np"),
@@ -36,23 +54,72 @@ def read_root_file(infile):
                 singles_tree["sourcePosX"].array(library="np"),
                 singles_tree["sourcePosY"].array(library="np"),
                 singles_tree["sourcePosZ"].array(library="np")), axis=-1))),
+            "energy": singles_tree["energy"].array(library="np"),
         })
-        coincidences = pd.DataFrame({
-            "time1": coincidence_tree["time1"].array(library="np"),
-            "time2": coincidence_tree["time2"].array(library="np"),
-            "detector1": coincidence_tree["crystalID1"].array(library="np"),
-            "detector2": coincidence_tree["crystalID2"].array(library="np"),
-            "source1": list(map(tuple, np.stack((
-                coincidence_tree["sourcePosX1"].array(library="np"),
-                coincidence_tree["sourcePosY1"].array(library="np"),
-                coincidence_tree["sourcePosZ1"].array(library="np")), axis=-1))),
-            "source2": list(map(tuple, np.stack((
-                coincidence_tree["sourcePosX2"].array(library="np"),
-                coincidence_tree["sourcePosY2"].array(library="np"),
-                coincidence_tree["sourcePosZ2"].array(library="np")), axis=-1))),
-        })
-        coincidences['true'] = coincidences['source1'] == coincidences['source2']
 
+    return singles
+
+
+def filter_singles(singles, energy_min=0.450, energy_max=0.750):
+    """
+        Energy-filter singles, parameters are in MeV, defaults 0.450 to 0.750 MeV
+        This is the energy range of the 511 keV gamma photons from positron annihilation
+        This is the range used in Oliver & Rafecas
+    """
+    singles = singles[singles['energy'] > energy_min].reset_index()
+    singles = singles[singles['energy'] < energy_max].reset_index()
+    return singles
+
+
+def bundle_coincidences(singles):
+    """
+        Bundle singles into coincidences, returns a DataFrame with columns:
+        time1, time2, detector1, detector2, source1, source2, true
+        where true is True if the two singles are from the same source, (true coincidence)
+        False otherwise
+    """
+    coins = []
+    window_start = 0
+    possible_coincidence = []
+    for i in range(0, len(singles['time']) - 1):
+        if singles['time'][i] - window_start >= TAU:
+            # process any previous coincidences
+            if len(possible_coincidence) >= 2:
+                # If there are at least 2 singles in the window, we have a possible coincidence
+                if len(possible_coincidence) > 2:
+                    energies = [singles['energy'][j] for j in possible_coincidence]
+                    energies = dict(zip(energies, possible_coincidence))
+                    true_coinc = (energies.pop(max(energies)), energies.pop(max(energies)))
+                    # Take just the two highest energies
+                    # takeWinnerOfGoods policy
+                else:
+                    true_coinc = tuple(possible_coincidence)
+                coins.append(true_coinc)
+
+            # Reset the window
+            possible_coincidence = [i]
+            window_start = singles['time'][i]
+        elif singles['time'][i] - window_start < TAU:
+            possible_coincidence.append(i)  # Add to coincidence list
+    coinci = pd.DataFrame()
+    coinci['time1'] = [singles['time'][i[0]] for i in coins]
+    coinci['time2'] = [singles['time'][i[1]] for i in coins]
+    coinci['detector1'] = [singles['detector'][i[0]] for i in coins]
+    coinci['detector2'] = [singles['detector'][i[1]] for i in coins]
+    coinci['source1'] = [singles['source'][i[0]] for i in coins]
+    coinci['source2'] = [singles['source'][i[1]] for i in coins]
+    coinci['true'] = coinci['source1'] == coinci['source2']
+    return coinci
+
+
+def sp_counts(singles, coincidences):
+    """
+        Calculate Singles-Prompts (SP) counts for singles and coincidences
+        Returns a tuple of two dicts: singles_count, prompts_count
+        singles_count is a dict of form {detectorID: count} for singles
+        prompts_count is a dict of form {detectorID: count} for prompts
+        where prompts are the sum of all coincidences involving a given detector
+    """
     # Calculate singles/prompts counts
     det1_counts = coincidences['detector1'].value_counts().to_dict()  # det1 coincidences involved
     det2_counts = coincidences['detector2'].value_counts().to_dict()  # det2 coincidences involved
@@ -69,12 +136,15 @@ def read_root_file(infile):
     # dicts of form {detectorID: count}
     prompts_count = prompts.set_index('detector')['prompts'].to_dict()
     singles_count = singles['detector'].value_counts().to_dict()
-    return singles, coincidences, singles_count, prompts_count
+
+    return singles_count, prompts_count
 
 
-# Returns the Singles-Prompts (SP) randoms rate estimate from a pair
-# of detectors with crystalIDs i and j per Oliver & Rafecas
 def randomsrate(i, j, singles_count, prompts_count, L, S):
+    """
+        Returns the Singles-Prompts (SP) randoms rate estimate from a pair
+        of detectors with crystalIDs i and j per Oliver & Rafecas
+    """
     # P_i and P_j are prompts rates for detectors i, j
     P_i = prompts_count.get(i, 0) / TIME  # .get(i, 0) returns 0 if det i not in prompts_count
     P_j = prompts_count.get(j, 0) / TIME
@@ -89,8 +159,10 @@ def randomsrate(i, j, singles_count, prompts_count, L, S):
     return coeff * i_term * j_term
 
 
-# Calculate Singles-Prompts (SP) estimate of total randoms rate
 def singles_prompts(singles_count, prompts_count, singles, coincidences):
+    """
+        Calculate Singles-Prompts (SP) estimate of total randoms rate
+    """
 
     S = len(singles) / TIME  # Rate of singles measured by scanner as a whole
     P = 2 * len(coincidences) / TIME  # Twice the prompts rate
@@ -113,9 +185,9 @@ def singles_prompts(singles_count, prompts_count, singles, coincidences):
 
 
 # Calculate delayed-window estimate of total randoms rate
-def delayed_window(coincidences, singles):
+def delayed_window(singles):
     dw_estimate = 0
-    for t in coincidences['time1']:
+    for t in singles['time']:
         dw_estimate += (
             np.searchsorted(singles['time'], t + DELAY + TAU) -
             np.searchsorted(singles['time'], t + DELAY)
@@ -141,12 +213,20 @@ if __name__ == "__main__":
     for i in FILE_RANGE:
         infile = PATH_PREFIX + str(i) + PATH_SUFFIX
         print(f"Reading file {infile}...")
-        singles, coincidences, singles_count, prompts_count = read_root_file(infile)
+        singles = read_root_file(infile)
+
+        if FILTER_ENERGY:
+            singles = filter_singles(singles)  # Filter singles by energy
+
+        coincidences = bundle_coincidences(singles)  # Bundle singles into coincidences
+        singles_count, prompts_count = sp_counts(singles, coincidences)
+
         sp.append(singles_prompts(singles_count, prompts_count, singles, coincidences))
-        dw.append(delayed_window(coincidences, singles))
+        dw.append(delayed_window(singles))
         sr.append(singles_rate(singles_count))
         actual.append(len(coincidences[~coincidences['true']]))
+        print(f"File {str(i)} processed. SP: {sp[-1]}, DW: {dw[-1]}, SR: {sr[-1]}, Actual: {actual[-1]}")
 
     df = pd.DataFrame({'sp': sp, 'dw': dw, 'sr': sr, 'actual': actual})
-    with open('estimations.csv', 'w') as f:
+    with open(OUTPUT_FILE, 'w') as f:
         df.to_csv(f)
