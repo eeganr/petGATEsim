c8cd4465ed3fd6c532cbeb82fffbb405b9b72190
Eegan Ram <67843748+eeganr@users.noreply.github.com>
2025-06-30 10:12:00 -0700

    c++ testing

--- estimations.cpp
+++ estimations.cpp
@@ -1,11 +1,12 @@
+#include <pybind11/pybind11.h>
 #include <iostream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
-int main() {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+int main(int argc, char *argv[]) {
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!", "meow"};
 
     for (const string& word : msg)
     {
